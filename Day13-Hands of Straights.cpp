dabb2925353251087e44d80609cc4cc534be025d
Roshan <roshangupta2020@gmail.com>
2023-05-20 13:15:44 +0530

    Day 13 done using map updated solution 2

--- Day13-Hands of Straights.cpp
+++ Day13-Hands of Straights.cpp
@@ -27,3 +27,28 @@ class Solution {
         return true;
     }
 };
+
+
+class Solution2 {
+  public:
+    bool isStraightHand(int N, int groupSize, vector<int> &hand) {
+        // code here
+        
+        if(N%groupSize  != 0) return false;
+        if(N==1 || groupSize==1) return true;
+        map<int , int>m;
+        for(int &x : hand) m[x]++;
+        
+        for(auto &pr : m){
+            int v = pr.first;
+            int c = pr.second;
+            if(c==0) continue;
+            for(int j=v+1;j< v + groupSize;j++){
+                if(m[j]==0 or m[j]<c ) return false;
+                m[j] -= c;
+            }
+        }
+        
+        return true;
+    }
+};
