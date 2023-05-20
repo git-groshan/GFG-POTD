722d35fe1b4d98057acdbd561cc49dd0a86c7d0c
Roshan <roshangupta2020@gmail.com>
2023-05-20 12:58:39 +0530

    Day 13 done using map

---
+++ Day13-Hands of Straights.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+//problem link
+//https://practice.geeksforgeeks.org/problems/hands-of-straights/1
+
+class Solution {
+  public:
+    bool isStraightHand(int N, int groupSize, vector<int> &hand) {
+        // code here
+        
+        if(N%groupSize  != 0) return false;
+        if(N==1 || groupSize==1) return true;
+        map<int , int>m;
+        for(int &x : hand) m[x]++;
+        
+        for(auto &pr : m){
+            int v = pr.first;
+            int c = pr.second;
+            if(c==0) continue;
+            for(int j=v;j< v + groupSize;j++){
+                if(m[j]==0 or m[j]<c ) return false;
+                m[j] -= c;
+            }
+        }
+        
+        return true;
+    }
+};
