22b8b8a16cd6b0bf91229fa23e2a7a6edc14dd1b
Roshan <roshangupta2020@gmail.com>
2023-05-21 13:17:25 +0530

    Day 14 Done in cpp

---
+++ Day14-Bus Conductor.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+//Problem link 
+//https://practice.geeksforgeeks.org/problems/c6ced401352fd126b89129cd562a9247f057e40e/1
+
+class Solution {
+  public:
+    int findMoves(int n, vector<int> chairs, vector<int> passengers) {
+        sort(chairs.begin() , chairs.end());
+        sort(passengers.begin() , passengers.end());
+        
+        int ans = 0;
+        for(int i=0;i<n;i++){
+            ans+= abs(chairs[i] - passengers[i]);
+        }
+        
+        return ans;
+        
+    }
+};
