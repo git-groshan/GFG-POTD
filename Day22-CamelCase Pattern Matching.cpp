b6805320303042067a2d295bb61cf051e99e2f74
Roshan <roshangupta2020@gmail.com>
2023-05-29 11:12:26 +0530

    Day 22 Medium done in java and cpp

---
+++ Day22-CamelCase Pattern Matching.cpp
@@ -0,0 +1,45 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+//Problem link 
+//https://practice.geeksforgeeks.org/problems/camelcase-pattern-matching2259/1
+
+bool func(string A,string B)
+    {
+        int i = 0;
+        int j = 0;
+        while(i<A.size() and j<B.size())
+        {
+            if(A[i] == B[j])
+            {
+                i++;
+                j++;
+            }
+            else if(A[i] >= 'A' and A[i]<='Z' and B[j]>='A' and B[j]<='Z')
+            {
+                return false;
+            }
+            else
+            {
+                i++;
+            }
+        }
+        return j==B.size();
+    }
+    vector<string> CamelCase(int N, vector<string> D, string Pattern) {
+        vector<string> ans;
+        for(auto c : D)
+        {
+            if(func(c,Pattern))
+            {
+                ans.push_back(c);
+            }
+        }
+        if(ans.empty())
+        {
+            vector<string> bools;
+            bools.push_back("-1");
+            return bools;
+        }
+        return ans;
+    }
